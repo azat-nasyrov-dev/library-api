439c62d4a6ec69110fe418b66b5037bbf2aa4861
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-06-19 13:39:29 +0600

    modified user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export const enum UserRole {
+  USER = 1 << 0,
+  ADMIN = 1 << 1,
+}
+
 @Entity({ name: 'users' })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
@@ -13,4 +18,7 @@ export class UserEntity {
 
   @Column({ name: 'password_hash', nullable: false, select: false })
   passwordHash: string;
+
+  @Column({ default: UserRole.USER })
+  roles: number;
 }
