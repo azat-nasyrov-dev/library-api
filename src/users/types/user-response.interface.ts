2f1c5e8174f2e17e263745336d9292a880ec76f6
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-06-19 13:29:58 +0600

    added another type

--- src/users/types/user-response.interface.ts
+++ src/users/types/user-response.interface.ts
@@ -1,5 +1,5 @@
-import { UserEntity } from '../entities/user.entity';
+import { UserType } from './user.type';
 
 export interface UserResponseInterface {
-  user: UserEntity & { token: string };
+  user: UserType & { token: string };
 }
